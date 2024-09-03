96b686dc19b3e17c0ba19ea0fd9a5208f4e8d743
DPigeon <DPigeon.W@Hotmail.com>
2024-09-02 23:35:44 -0400

    Update release workflow with finding changes

--- release/find-and-apply-changes.sh
+++ release/find-and-apply-changes.sh
@@ -7,8 +7,14 @@
 # Doing some bash operations gives us the most recent updated and removed files
 ###
 
+PUBLIC_REPO_PATH=$1
 CHANGES_FILE=changes.txt
 CHANGES_FINAL_FILE=changes-final.txt
+
+if [ -z "$PUBLIC_REPO_PATH" ] || [ ! -d "$PUBLIC_REPO_PATH" ]; then 
+    echo "Wrong public repository path: $PUBLIC_REPO_PATH" 
+    exit 1
+fi
 
 SINCE=$(curl -s https://api.github.com/repos/JSsixtyfour/smashremix/releases/latest \
         | grep '"published_at":' \
@@ -23,23 +29,24 @@ DELETED_FILES=()
 while IFS=$'\t' read -r change_type file_name; do
     if [[ $change_type =~ A|M ]]; then
         UPDATED_FILES+=("$file_name")
+    elif [[ $change_type == D ]]; then
+        DELETED_FILES+=("$PUBLIC_REPO_PATH/$file_name")
     else
-        DELETED_FILES+=("$file_name")
+        continue
     fi
 done < $CHANGES_FINAL_FILE
 
-# TODO:
-# 1. Checkout from current repo
-    #   uses: actions/checkout@v3
+rm -rf $CHANGES_FILE $CHANGES_FINAL_FILE
 
-# 2:
-# git config user.name "github-actions"
-# git config user.email "actions@github.com"
-# git remote add destination https://github.com/JSsixtyfour/smashremix.git
+echo "Updating `${UPDATED_FILES[@]}` in public repo..."
 
-# 3. Push to destination repo
-    #   - name: Push to Public Repository
-    #     run: |
-    #       git push destination main
-    #     env:
-    #       GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}    
+### DELETE AFTER TEST
+cat '123' > test1.txt
+cat '1234' > test2.txt
+cp -pr test1.txt test2.txt $PUBLIC_REPO_PATH/
+### DELETE AFTER TEST
+
+#cp -pr ${UPDATED_FILES[@]} $PUBLIC_REPO_PATH/
+
+echo "Removing `${DELETED_FILES[@]}` from public repo..."
+#rm -rf ${DELETED_FILES[@]}
